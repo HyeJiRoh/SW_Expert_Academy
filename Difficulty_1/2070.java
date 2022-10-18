f60e1484822b803b0c0bc7ff8271966a7cb4f4f2
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-18 22:23:36 +0900

    2070. 큰 놈, 작은 놈, 같은 놈

---
+++ Difficulty_1/2070.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.math.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i=1; i<=T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a>b) {
+				System.out.println("#" + i + " >");
+			} else if(a==b) {
+				System.out.println("#" + i + " =");
+			} else {
+				System.out.println("#" + i + " <");
+			}
+		}
+	}
+}
