197418a7363754e6b7ac6c611bb7c63966fe0dc6
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-19 22:56:56 +0900

    2058. 자릿수 더하기

--- Difficulty_1/2058.java
+++ Difficulty_1/2058.java
@@ -1,21 +1,17 @@
 import java.util.*;
 
-public class D1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
 		int total = 0;
 
-		System.out.println("N : "+N);
-		
 		for(int i=0; i<5; i++) {
 			total += N%10;
 			N = N/10;
-			System.out.println("new N : "+N);
-			System.out.println("total : "+total);
 		}
 		System.out.println(total);
 	}
