728ec91f8e9e1579389ce9548ca7b4cdfcc4e1a0
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-19 22:40:00 +0900

    2046. 스탬프 찍기

---
+++ 2046.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		for(int i=0; i<input; i++) {
+			System.out.print("#");
+		}
+	}
+}
