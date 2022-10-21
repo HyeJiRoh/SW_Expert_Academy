54c26ca810b8d8c5c995647426c4040e73a872a5
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-22 05:14:58 +0900

    1933. 간단한 N 의 약수

---
+++ Difficulty_1/1933.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+         
+        int input = sc.nextInt();
+         
+        for(int i=1; i<=input; i++) {
+            if(input%i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
